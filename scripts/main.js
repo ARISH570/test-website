ea8c27dde8ac3859c2b6dba5d5cf766c82101dbf
ARISH570 <177727412+ARISH570@users.noreply.github.com>
2025-01-25 18:05:27 +0530

    Initial commit for website

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+/*const myHeading = document.querySelector("p");
+myHeading.textContent = "Hello world!";
+
+ document.querySelector("html").addEventListener("click", function () {
+    alert("Ouch! Stop poking me!");
+  });
+
+
+  
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result;
+  }
+  console.log(multiply(4, 7));
+  multiply(20, 20);
+  multiply(0.5, 3);*/
+
+  
