d94b3eb36aa24f42786e2c7196721bb28d938c69
ARISH570 <177727412+ARISH570@users.noreply.github.com>
2025-01-25 18:11:36 +0530

    main.js file modified for testing

--- scripts/main.js
+++ scripts/main.js
@@ -14,5 +14,7 @@ function multiply(num1, num2) {
   console.log(multiply(4, 7));
   multiply(20, 20);
   multiply(0.5, 3);*/
+  document.querySelector("html").addEventListener("click", function () {
+    alert("Ouch! Stop poking me!");
+  });
 
-  
